eae39959f07b6f644e50f463c399baa6fc7bc7f7
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2024-12-06 12:49:02 +0600

    add a word to guess

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        String word1 = "word";
+        String[] word1Array = {"w", "o", "r", "d"};
+        String[] visibleWord = new String[word1Array.length];
+
+        for (int i = 0; i < word1Array.length; i++) {
+            visibleWord[i] = "_ ";
+            System.out.print(visibleWord[i]);
+        }
+        System.out.println();
+
+        int rightGuessCount = 0;
+        outerLoop:
+        while (rightGuessCount < visibleWord.length) {
+            String usersGuess = sc.nextLine();
+
+            for (int i = 0; i < word1Array.length; i++) {
+                if (Objects.equals(word1Array[i], usersGuess)) {
+                    visibleWord[i] = usersGuess + " ";
+                    rightGuessCount++;
+                    word1Array[i] = "guessed";
+                }
+                else if (Objects.equals(word1, usersGuess)) {
+                    System.out.println("w o r d");
+                    break outerLoop;
+                }
+                System.out.print(visibleWord[i]);
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
